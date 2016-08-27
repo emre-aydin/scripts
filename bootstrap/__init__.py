1b66891ccfeec3e87eb27983b471654768fa4b34
emre <emre@aydin.tech>
2016-08-27 17:46:04 +0300

    Add configure_locales operation

--- bootstrap/__init__.py
+++ bootstrap/__init__.py
@@ -3,9 +3,9 @@ import sys
 
 
 def configure_passwordless_sudo(args):
+    _ensure_root_user()
+
     username = args.username
-    if os.geteuid() != 0:
-        sys.exit("Need to run as root")
 
     cmd = "%s ALL=(ALL) NOPASSWD:ALL" % username
     file_path = os.path.join("/etc/sudoers.d", username)
@@ -20,3 +20,30 @@ def configure_passwordless_sudo(args):
     with open(file_path, mode='a') as fp:
         fp.write(cmd)
         print("Allowed passwordless sudo access for %s" % username)
+
+
+def configure_locales():
+    _ensure_root_user()
+
+    with open("/etc/default/locale", mode="w") as fp:
+        fp.write("""LANG="en_US.UTF-8"
+LANGUAGE="en_US.UTF-8"
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"
+""")
+
+
+def _ensure_root_user():
+    if os.geteuid() != 0:
+        sys.exit("Need to run as root")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from bootstrap import configure_passwordless_sudo
+from bootstrap import configure_passwordless_sudo, configure_locales
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,6 +10,9 @@ if __name__ == "__main__":
     config_sudo_parser = subparsers.add_parser("config-sudo", help="Allow user to use sudo without password")
     config_sudo_parser.add_argument("username", help="Username to allow passwordless sudo")
     config_sudo_parser.set_defaults(func=configure_passwordless_sudo)
+
+    locale_parser = subparsers.add_parser("config-locales", help="Configures all system locales to 'en_US.UTF-8'")
+    locale_parser.set_defaults(func=configure_locales)
 
     args = parser.parse_args()
     args.func(args)
